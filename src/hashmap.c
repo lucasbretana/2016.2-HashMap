5d56bcff712687b133dbabcd974f8efff59f80fc
Lucas E. Bretana <lucasbretana@gmail.com>
2016-10-10 18:46:30 -0300

    Initial fase. NOTE: aparently you cannot override the function strcmp, it stops giving warning, about implicit declararion, but uses the one defines on string.h

---
+++ src/hashmap.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include "hashmap.h"
+
+unsigned int SIZE = TAM_INI;
+
+int length(key *k){
+  return 0;
+}
+
+// Reference: http://eternallyconfuzzled.com/tuts/algorithms/jsw_tut_hashing.aspx
+pos h(void *k, int len){
+  unsigned char *p = k;
+  unsigned h = 2166136261;
+  int i;
+  for (i = 0; i < len; i++){
+    h = (h * 16777619) ^ p[i];
+  }
+  return h;
+}
+
+pos h1(key *k){
+  return h(k, length(k)) % SIZE;
+}
+
+pos h2(key *k){
+  return 1 + ( h(k, length(k)) % (SIZE - 1) );
+}
---
+++ src/hashmap.h
@@ -0,0 +1,9 @@
+#define TAM_INI 500
+#define ALPHA 0.75
+
+typedef unsigned int pos;
+typedef char key;
+
+pos h(void *, int);
+pos h1(key *);
+pos h2(key *);
