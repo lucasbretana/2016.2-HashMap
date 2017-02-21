cf55c03e09efa052a8636cdd6d8996b5dff535a1
Lucas Eduardo Bretana <lucasbretana@gmail.com>
2017-02-21 19:47:31 -0300

    No ideia what Jon has changed on this file, so better keep it safe

--- src/hashmap.c
+++ src/hashmap.c
@@ -30,18 +30,50 @@ position_t position(key_p k){
   return 0;
 }
 
-void hash_insert(HashMap_t *hash, key_p hashKey){
-}
+/**
+ * If it is Chaining, then to remove it must find in the list
+ * If is is Linear then is start to look in the hash list, stop when you foun it, work it is not in the list
+ */
 void hash_delete(HashMap_t *hash, key_p hashKey){
-}
-void hash_get(HashMap_t *hash, key_p hashKey){
-  if(hash->method != Chaining)
-    if(strcomp(((key_p) hash->keys[h1(hashKey, length(hashKey))]), hashKey) == 0){
-        // FIND IT0
-        // No confl0icts, first try
-        printf("Found it");
+  position_t startPoint = 0;
+  switch(hash->method){
+    case Chaining:
+      break;
+    case Linear:
+      startPoint = h1(hashKey, length(hashKey));
+      position_t current = startPoint;
+      if(strcomp(((key_p)hash->keys) + startPoint, hashKey) != 0){
+        // You lucky bastard. Found on first try
+        key_p *deletedEntry = ((key_p *)hash->keys) + startPoint;
+        free(*(((key_p *)hash->keys) + startPoint));
+        *deletedEntry = NULL;
+      }else{
+        // Haha gonna have to look all over the hash
+        do{
+          // You lucky bastard. Found on first try
+          key_p *deletedEntry = ((key_p *)hash->keys) + startPoint;
+          free(*(((key_p *)hash->keys) + startPoint));
+          *deletedEntry = NULL;
+        }while((strcomp(((key_p)hash->keys) + current, hashKey) != 0) && (strcomp(((key_p)hash->keys) + current, ((key_p)hash->keys) + startPoint) == 0));
+      }
+      break;
+    case Quadratic:
+      break;
+    case Double_Hash:
+      break;
+    default:
+      fprintf(stderr, "There was something wrong! The conflict methodis not valid!\n");
+      break;
     }
 }
+// void hash_get(HashMap_t *hash, key_p hashKey){
+//   if(hash->method != Chaining)
+//     if(strcomp(((key_p) hash->keys[h1(hashKey, length(hashKey))]), hashKey) == 0){
+//         // FIND IT0
+//         // No confl0icts, first try
+//         printf("Found it");
+//     }
+// }
 
 HashMap_t *hash_initialize(ConflictMethods_t method){
     HashMap_t *h = malloc(sizeof(HashMap_t));
